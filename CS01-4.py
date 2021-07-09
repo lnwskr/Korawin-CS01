2fde69054b9eae6ca029ef950a1327a5a6626184
lnwskr <86812975+lnwskr@users.noreply.github.com>
2021-07-09 09:45:36 +0700

    Update CS01-4.py

--- CS01-4.py
+++ CS01-4.py
@@ -1,25 +1,21 @@
-# A = int(input("A = "))
-# B = int(input("B = "))
-# C = A+B
-# print(C)
+A = int(input("A = "))
+B = int(input("B = "))
+C = A+B
+print(C)
 
-# A = int(input("A = "))
-# B = int(input("B = "))
-# C = A-B
-# print(C)
+A = int(input("A = "))
+B = int(input("B = "))
+C = A-B
+print(C)
 
-# A = int(input("A = "))
-# B = int(input("B = "))
-# C = A*B
-# print(C)
+A = int(input("A = "))
+B = int(input("B = "))
+C = A*B
+print(C)
 
-# A = int(input("A = "))
-# B = int(input("B = "))
-# C = A/B
-# print(C)
+A = int(input("A = "))
+B = int(input("B = "))
+C = A/B
+print(C)
 
 
-print(100+5)
-print(100-5)
-print(100*5)
-print(100/5)
