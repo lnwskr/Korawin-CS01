3c06b5ce85bf160f2682a500df4054afab944d5a
lnwskr <86812975+lnwskr@users.noreply.github.com>
2021-07-09 09:40:27 +0700

    Create CS01-4.py

---
+++ CS01-4.py
@@ -0,0 +1,19 @@
+A = int(input("A = "))
+B = int(input("B = "))
+C = A+B
+print(C)
+
+A = int(input("A = "))
+B = int(input("B = "))
+C = A-B
+print(C)
+
+A = int(input("A = "))
+B = int(input("B = "))
+C = A*B
+print(C)
+
+A = int(input("A = "))
+B = int(input("B = "))
+C = A/B
+print(C)
