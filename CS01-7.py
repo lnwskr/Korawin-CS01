1793f9ae6731b3378be9a52394c1d7ee1035c084
lnwskr <86812975+lnwskr@users.noreply.github.com>
2021-07-16 09:25:45 +0700

    Create CS01-7.py

---
+++ CS01-7.py
@@ -0,0 +1,8 @@
+for i in range(3):
+    A = int(input("enter = "))
+    if(A <= 10):
+        print("ไม่ผ่าน")
+    elif(A <= 20):
+        print("ปรับปรุง")
+    elif(A <= 30):
+        print("ดีมาก")
